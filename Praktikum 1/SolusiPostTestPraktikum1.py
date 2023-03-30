25e67dc4c0e4acdd8803e13d2393758283049319
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-03-30 11:14:19 +0700

    update praktikum 1

---
+++ Praktikum 1/SolusiPostTestPraktikum1.py
@@ -0,0 +1,30 @@
+def mean(n):
+    sum = 0
+    for i in range(len(n)):
+        sum = sum + int(n[i])
+    return round(sum/len(n), 2)
+
+def median(n):
+    myList = [int(i) for i in n]
+    myList.sort()
+    return myList[int(len(n)/2)]
+
+def modus(n):
+    myDict = {}
+    for i in n:
+        if i not in myDict.keys():
+            myDict[i] = n.count(i)
+    
+    result = []
+    for key, value in myDict.items():
+        if value == max(myDict.values()):
+            result.append(key)
+    return result
+
+N = input()
+meanResult = mean(N)
+medianResult = median(N)
+modusResult = modus(N)
+print(f"Mean: {meanResult}")
+print(f"Median: {medianResult}")
+print(f"Modus: {' '.join(modusResult)}")
